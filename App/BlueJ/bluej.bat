a474768ae0348f345990f803533ae16af5c5ab55
Matt Nadareski <mnadareski@gmail.com>
2013-10-23 20:48:52 -0400

    3.0.6 Dev Test 2

--- App/BlueJ/bluej.bat
+++ App/BlueJ/bluej.bat
@@ -1,4 +1,4 @@
 @echo off
 set APPBASE="%CD%\App\BlueJ"
 set CP=%APPBASE%\lib\bluej.jar;%APPBASE%\lib\openjdk-6.0.21\lib\tools.jar
-%APPBASE%\lib\openjdk-6.0.21\bin\java.exe -cp %CP% bluej.Boot  %1 %2 %3 %4 %5 %6 %7 %8 %9
+%APPBASE%\lib\java-se-7-ri\bin\java.exe -cp %CP% bluej.Boot  %1 %2 %3 %4 %5 %6 %7 %8 %9
